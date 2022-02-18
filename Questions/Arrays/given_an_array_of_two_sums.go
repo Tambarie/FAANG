38e993a96453045640dc259826ad072cb6ea1e23
tambarie <gt.gbaragbo@gmail.com>
2022-02-18 22:03:05 +0100

    TwoSums solved

--- Questions/Arrays/given_an_array_of_two_sums.go
+++ Questions/Arrays/given_an_array_of_two_sums.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func TwoSums(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for k, value := range nums {
+		if idx, ok := m[target-value]; ok {
+			return []int{idx, k}
+		}
+		m[value] = k
+	}
+	return nil
+}
+
+func main() {
+	arr := []int{1, 24, 5, 3, 2}
+	fmt.Println(TwoSums(arr, 5))
+}
